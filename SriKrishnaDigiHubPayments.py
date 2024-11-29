f1987969a7ba99b4a741e4852bde7b9fbde37def
Hari DevX <106622827+HariDama21@users.noreply.github.com>
2024-11-29 23:38:00 +0530

    Update SriKrishnaDigiHubPayments.py
    Sample3

--- SriKrishnaDigiHubPayments.py
+++ SriKrishnaDigiHubPayments.py
@@ -7,12 +7,10 @@ st.set_page_config(page_title="Sri Krishna Digital Hub Payments", page_icon=":mo
 st.title("Welcome to Sri Krishna Digital Hub Payments")
 st.write("Click the button below to proceed with the payment.")
 
-# Predefined UPI Payment URL
-upi_url = "upi://pay?pa=8367772921@postbank&pn=DAMA%20HARI&cu=INR&mode=02&orgId=159768&sign=MEYCIQD+XkSoNXcZ87f48VnmF4Tbx4zVn3xuQ0kqBheTiQ8OOQIhAOgV2H8VhcLrSCetd92zEnLd/mYVQm4CyAkxbmsNOXKS"
 
 # HTML button to trigger the UPI payment link
 st.markdown(f'''
-    <a href="{upi_url}" target="_blank">
+    <a href="upi://pay?pa=8367772921@postbank&pn=DAMA%20HARI&cu=INR&mode=02&orgId=159768&sign=MEYCIQD+XkSoNXcZ87f48VnmF4Tbx4zVn3xuQ0kqBheTiQ8OOQIhAOgV2H8VhcLrSCetd92zEnLd/mYVQm4CyAkxbmsNOXKS" target="_blank">
         <button style="background-color: #008CBA; color: white; font-size: 16px; padding: 10px 20px; border-radius: 5px; border: none; cursor: pointer;">
             Click to Pay
         </button>
